289bbae15646c621f7904a5443437609ae980fc2
Kent Hong <kent.n.hong@gmail.com>
2025-09-02 20:57:46 -0700

    Initial commit

---
+++ LTR-329.h
@@ -0,0 +1,54 @@
+/**
+ * @file LTR-329.h
+ * @brief Header file for the LTR-329 ambient light sensor driver.
+ * @author Kent Hong
+ *
+ * This file contains definitions and function prototypes for initializing
+ * and interacting with the LTR-329 sensor over I2C.
+ *
+ * @note Ensure that the I2C peripheral is properly configured before using this driver.
+ */
+
+#ifndef INC_LTR_329_H_
+#define INC_LTR_329_H_
+
+#include <stdint.h>
+#include "stm32L4xx_hal.h" // Adjust this include based on your STM32 series
+
+/** @brief Register Defines for LTR-329 Ambient Light Sensor */
+#define LTR_329_I2C_ADDR (0x29 << 1) // LTR-329 I2C address shifted for HAL
+#define LTR_329_PART_ID_ADDR 0x86    // Device ID
+#define LTR_329_ALS_CONTR 0x80       // ALS Control Register
+#define LTR_329_ALS_MEAS_RATE 0x85   // ALS Measurement Rate Register
+#define LTR_329_MANUFAC_ID 0x87		 // Manufacturer ID Register
+#define LTR_329_ALS_DATA_CH1_0 0x88  // ALS Data Channel 1 Low Byte
+#define LTR_329_ALS_DATA_CH1_1 0x89  // ALS Data Channel 1 High Byte
+#define LTR_329_ALS_DATA_CH0_0 0x8A  // ALS Data Channel 0 Low Byte
+#define LTR_329_ALS_DATA_CH0_1 0x8B  // ALS Data Channel 0 High Byte
+#define LTR_329_ALS_STATUS 0x8C      // ALS Status Register
+
+#define LTR_329_PART_ID 0xA0 // LTR-329 Part ID Default Value (0xA0 -> 1010)
+
+/** @brief Struct to store LTR-329 variables */
+typedef struct {
+	uint16_t c0Data;           // Variable to store C0 channel data
+	uint16_t c1Data;           // Variable to store C0 and C1 channel data
+	uint8_t alsGainDataBinary; // Variable to store sensor binary gain data
+	uint8_t alsGainData;       // Variable to store gain setting
+	uint8_t alsIntDataBinary;  // Variable to store sensor binary integration time data
+	uint16_t alsIntData;       // Variable to store integration time setting
+	float alsLuxData;          // Variable to store calculated lux value
+} LTR329_t;
+
+/** @brief Function Prototypes for LTR-329 ALS */
+uint8_t LTR_329_Init(I2C_HandleTypeDef *hi2c);
+uint8_t LTR_329_Reset(I2C_HandleTypeDef *hi2c);
+HAL_StatusTypeDef LTR_329_RegWrite(uint8_t regAddr, uint8_t regData);
+HAL_StatusTypeDef LTR_329_RegRead(uint8_t regAddr, uint8_t *regData);
+uint16_t LTR_329_Read_C1(I2C_HandleTypeDef *hi2c);
+uint16_t LTR_329_Read_C0(I2C_HandleTypeDef *hi2c);
+uint8_t LTR_329_Read_ALS_Gain(I2C_HandleTypeDef *hi2c);
+uint8_t LTR_329_Read_ALS_Int(I2C_HandleTypeDef *hi2c);
+float LTR_329_Calculate_Lux(uint16_t c0Data, uint16_t c1Data, uint8_t alsGainData, uint8_t alsIntData);
+
+#endif /* INC_LTR_329_H_ */
---
+++ main.c
@@ -0,0 +1,428 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "LTR-329.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+LTR329_t ltr329; // Create an instance of the LTR-329 struct
+char buffer[128]; // Buffer for UART transmission
+const uint8_t gainMap[] = {1, 2, 4, 8, 48, 96}; // Gain mapping in pg. 13 of LTR-329 datasheet
+const uint16_t intTimeMap[] = {100, 50, 200, 400, 150, 250, 300, 350}; // Integration time mapping in pg. 14 of LTR-329 datasheet
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_I2C1_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  MX_I2C1_Init();
+  /* USER CODE BEGIN 2 */
+  uint8_t sunSensorInitStatus = LTR_329_Init(&hi2c1); // Initialize the LTR-329 sensor
+  if (sunSensorInitStatus != 0) {
+	  sprintf(buffer, "LTR-329 Initialization Error: %d\r\n", sunSensorInitStatus);
+	  HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY); // Transmit error message over UART
+  }
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+	  ltr329.c0Data = LTR_329_Read_C0(&hi2c1); // Read C0 channel data
+	  if (ltr329.c0Data == 1) {
+		  sprintf(buffer, "I2C Read Error: %d\r\n", ltr329.c0Data);
+		  HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY); // Transmit error message over UART
+	  }
+
+	  ltr329.c1Data = LTR_329_Read_C1(&hi2c1); // Read C1 channel data
+	  if (ltr329.c1Data == 1) {
+		  sprintf(buffer, "I2C Read Error: %d\r\n", ltr329.c1Data);
+		  HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY); // Transmit error message over UART
+	  }
+
+	  ltr329.alsGainDataBinary = LTR_329_Read_ALS_Gain(&hi2c1); // Read ALS gain setting
+	  if (ltr329.alsGainDataBinary == 1) {
+		  sprintf(buffer, "I2C Read Error: %d\r\n", ltr329.alsGainDataBinary);
+		  HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY); // Transmit error message over UART
+	  }
+
+	  // Map the binary gain data to actual gain values
+	  switch (ltr329.alsGainDataBinary) {
+	  	  case 0: // b'000
+	  		  ltr329.alsGainData = gainMap[0]; // 1x gain
+	  		  break;
+	  	  case 1: // b'001
+	  		  ltr329.alsGainData = gainMap[1]; // 2x gain
+	  		  break;
+	  	  case 2: // b'010
+	  		  ltr329.alsGainData = gainMap[2]; // 4x gain
+	  		  break;
+	  	  case 3: // b'011
+	  		  ltr329.alsGainData = gainMap[3]; // 8x gain
+	  		  break;
+	  	  case 6: // b'110
+	  		  ltr329.alsGainData = gainMap[4]; // 48x gain
+	  		  break;
+	  	  case 7: // b'111
+	  		  ltr329.alsGainData = gainMap[5]; // 96x gain
+	  		  break;
+	  	  default:
+	  		  ltr329.alsGainData = 0; // Invalid gain setting
+	  		  sprintf(buffer, "Invalid ALS Gain Data: %u\r\n", ltr329.alsGainDataBinary);
+	  		  HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY); // Transmit error message over UART
+	  		  break;
+	  }
+
+
+	  ltr329.alsIntDataBinary = LTR_329_Read_ALS_Int(&hi2c1); // Read ALS integration time
+	  if (ltr329.alsIntDataBinary == 1) {
+		  sprintf(buffer, "I2C Read Error: %d\r\n", ltr329.alsIntDataBinary);
+		  HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY); // Transmit error message over UART
+	  }
+
+	  // Map the binary integration time data to actual integration time values
+	  switch (ltr329.alsIntDataBinary) {
+	  	  case 0:
+	  		  ltr329.alsIntData = intTimeMap[0]; // 100 ms integration time
+	  		  break;
+	  	  case 1:
+	  		  ltr329.alsIntData = intTimeMap[1]; // 50 ms integration time
+	  		  break;
+	  	  case 2:
+	  		  ltr329.alsIntData = intTimeMap[2]; // 200 ms integration time
+	  		  break;
+	  	  case 3:
+	  		  ltr329.alsIntData = intTimeMap[3]; // 400 ms integration time
+	  		  break;
+	  	  case 4:
+	  		  ltr329.alsIntData = intTimeMap[4]; // 150 ms integration time
+	  		  break;
+	  	  case 5:
+	  		  ltr329.alsIntData = intTimeMap[5]; // 250 ms integration time
+	  		  break;
+	  	  case 6:
+	  		  ltr329.alsIntData = intTimeMap[6]; // 300 ms integration time
+	  		  break;
+	  	  case 7:
+	  		  ltr329.alsIntData = intTimeMap[7]; // 350 ms integration time
+	  		  break;
+	  	  default:
+	  		  ltr329.alsIntData = 0; // Invalid integration time setting
+	  		  sprintf(buffer, "Invalid ALS Integration Time Data: %u\r\n", ltr329.alsIntDataBinary);
+	  		  HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY); // Transmit error message over UART
+	  }
+
+	  ltr329.alsLuxData = LTR_329_Calculate_Lux(ltr329.c0Data, ltr329.c1Data, ltr329.alsGainData, ltr329.alsIntData); // Calculate lux value
+
+	  // Code for debugging C0 data, C1 data, gain, and integration time
+	  //sprintf(buffer, "Raw C0: %u, Raw C1: %u, Gain: %u, Integration Time: %u\r\n", ltr329.c0Data, ltr329.c1Data, ltr329.alsGainData, ltr329.alsIntData); // Format data for transmission
+	  //HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
+
+	  sprintf(buffer, "Lux: %.2f\r\n", ltr329.alsLuxData); // Format lux value for transmission
+	  HAL_UART_Transmit(&huart2, (uint8_t *)buffer, strlen(buffer), HAL_MAX_DELAY); // Transmit lux value over UART
+
+	  HAL_Delay(600); // Delay for 600 ms to allow sensor to stabilize and avoid flooding UART
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 10;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.Timing = 0x10D19CE4;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB10 PB11 */
+  GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_11;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF4_I2C2;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
